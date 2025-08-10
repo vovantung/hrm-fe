20d653c4cbe1a92e7f87d5f000ac53891edb3904
vovantung <vovantungdt123@gmail.com>
2025-08-10 21:53:36 +0700

    cm13

--- src/components/layout/vertical/VerticalMenu.tsx
+++ src/components/layout/vertical/VerticalMenu.tsx
@@ -89,7 +89,7 @@ const VerticalMenu = ({ dictionary, scrollMenu }: Props) => {
           <MenuItem href={`/${locale}/admin/department`} icon={<i className='mingcute--department-line' />}>
             {dictionary['navigation'].department}
           </MenuItem>
-          <MenuItem href={`/${locale}/admin/weekly-report`} icon={<i className='mdi--report-box-outline' />}>
+          <MenuItem href={`/${locale}/admin/weekly-report`} icon={<i className='ix--report-barchart' />}>
             {dictionary['navigation'].weeklyReport}
           </MenuItem>
         </SubMenu>

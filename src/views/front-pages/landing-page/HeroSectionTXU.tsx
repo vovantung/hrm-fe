7673b06128255ba77917232dac6fa78e970ead9b
vovantung <vovantungdt123@gmail.com>
2026-04-18 17:04:29 +0700

    cm184

--- src/views/front-pages/landing-page/HeroSectionTXU.tsx
+++ src/views/front-pages/landing-page/HeroSectionTXU.tsx
@@ -80,9 +80,9 @@ const HeroSectionTXU = ({ mode }: { mode: SystemMode }) => {
         })}
       />
       <div className={classnames('pbs-[88px] overflow-hidden', frontCommonStyles.layoutSpacing)}>
-        <div className='md:max-is-[650px] mbs-0 mbe-7 mli-auto text-center relative'>
+        <div className='md:max-is-[600px] mbs-0 mbe-7 mli-auto text-center relative'>
           <Typography
-            className={classnames('font-extrabold sm:text-[26px] text-3xl mbe-4 leading-[26px]', styles.heroText)}
+            className={classnames('font-extrabold sm:text-[28px] text-3xl mbe-4 leading-[32px]', styles.heroText)}
           >
             Nền tảng quản lý doanh nghiệp hiện đại trên hạ tầng cloud native riêng biệt
           </Typography>

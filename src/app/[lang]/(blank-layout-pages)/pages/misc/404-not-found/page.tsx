38b34bf0549f5344bde840f5f41a2fc73f02cf28
vovantung <vovantungdt123@gmail.com>
2026-04-18 11:30:13 +0700

    cm164

--- src/app/[lang]/(blank-layout-pages)/pages/misc/404-not-found/page.tsx
+++ src/app/[lang]/(blank-layout-pages)/pages/misc/404-not-found/page.tsx
@@ -2,13 +2,15 @@
 import NotFound from '@views/NotFound'
 
 // Server Action Imports
-import { getServerMode } from '@core/utils/serverHelpers'
+// import { getServerMode } from '@core/utils/serverHelpers'
 
 const Error = async () => {
   // Vars
-  const mode = await getServerMode()
+  // const mode = await getServerMode()
 
-  return <NotFound mode={mode} />
+  return <NotFound />
+
+  // return <NotFound mode={mode} />
 }
 
 export default Error

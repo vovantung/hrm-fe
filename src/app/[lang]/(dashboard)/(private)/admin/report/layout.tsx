34beed0e6dad99b18b1b3e37c352cf6dd67985ea
vovantung <vovantungdt123@gmail.com>
2026-04-19 18:10:35 +0700

    cm210

--- src/app/[lang]/(dashboard)/(private)/admin/report/layout.tsx
+++ src/app/[lang]/(dashboard)/(private)/admin/report/layout.tsx
@@ -60,7 +60,7 @@ export default function PostLayout({ children }: { children: React.ReactNode }) 
       {/* Content page */}
       <div
         style={{
-          top: settings.layout == 'horizontal' ? '109px' : '70px',
+          top: settings.layout == 'horizontal' ? '111px' : '72px',
           transition: 'transform 0.2s ease-in-out, opacity 0.2s ease-in-out',
           transform: isVisible ? 'translateY(0)' : 'translateY(-56px)'
         }}
@@ -69,7 +69,7 @@ export default function PostLayout({ children }: { children: React.ReactNode }) 
           style={{
             position: 'fixed',
             width: '100%',
-            paddingRight: '396px',
+            paddingRight: '400px',
             zIndex: 9999
 
             // maxHeight: settings.layout == 'horizontal' ? 'calc(100vh - 185px)' : 'calc(100vh - 135px)',
@@ -86,7 +86,7 @@ export default function PostLayout({ children }: { children: React.ReactNode }) 
       <div
         style={{
           position: 'fixed',
-          top: settings.layout == 'horizontal' ? '109px' : '70px',
+          top: settings.layout == 'horizontal' ? '111px' : '72px',
           width: '420px',
           left: left,
           transition: 'transform 0.2s ease-in-out, opacity 0.2s ease-in-out',

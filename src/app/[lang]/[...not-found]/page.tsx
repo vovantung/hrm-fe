3e63d1578b930afeb86ecc07314051dd9fdc1459
vovantung <vovantungdt123@gmail.com>
2026-04-19 09:26:09 +0700

    cm193

--- src/app/[lang]/[...not-found]/page.tsx
+++ src/app/[lang]/[...not-found]/page.tsx
@@ -5,6 +5,7 @@ import type { Locale } from '@configs/i18n'
 import Providers from '@components/Providers'
 import BlankLayout from '@layouts/BlankLayout'
 import NotFound from '@views/NotFound'
+import themeConfig from '@/configs/themeConfig'
 
 // Config Imports
 import { i18n } from '@configs/i18n'
@@ -21,10 +22,11 @@ const NotFoundPage = async (props: { params: Promise<{ lang: Locale }> }) => {
 
   // const mode = await getServerMode()
   const systemMode = await getSystemMode()
+  const layoutMode = themeConfig.layout
 
   return (
     <Providers direction={direction}>
-      <BlankLayout systemMode={systemMode}>
+      <BlankLayout systemMode={systemMode} layoutMode={layoutMode}>
         {/* <NotFound mode={mode} /> */}
         <NotFound />
       </BlankLayout>

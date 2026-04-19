dbed004e6db26b489c79e5c75c27d1a65dcd055b
vovantung <vovantungdt123@gmail.com>
2026-04-19 18:54:27 +0700

    cm213

--- src/views/admin/report/WeeklyReport.tsx
+++ src/views/admin/report/WeeklyReport.tsx
@@ -454,7 +454,7 @@ const WeeklyReportView = () => {
         >
           <div
             style={{
-              height: settings.layout == 'horizontal' ? 'calc(100vh - 266px)' : 'calc(100vh - 226px)',
+              height: settings.layout == 'horizontal' ? 'calc(100vh - 262px)' : 'calc(100vh - 222px)',
               minHeight: '114px'
             }}
           >
@@ -462,7 +462,7 @@ const WeeklyReportView = () => {
               style={{
                 display: 'flex',
                 justifyContent: 'center',
-                maxHeight: settings.layout == 'horizontal' ? 'calc(100vh - 359px)' : 'calc(100vh - 318px)',
+                maxHeight: settings.layout == 'horizontal' ? 'calc(100vh - 355px)' : 'calc(100vh - 314px)',
                 minHeight: settings.layout == 'horizontal' ? '23px' : '23px',
                 overflowY: 'auto',
 

0a60226891461f2f72aa4c77815dfb56da32bfc0
vovantung <vovantungdt123@gmail.com>
2026-04-19 11:30:24 +0700

    cm201

--- src/configs/themeConfig.ts
+++ src/configs/themeConfig.ts
@@ -64,7 +64,7 @@ const themeConfig: Config = {
   mode: 'system', // 'system', 'light', 'dark'
   skin: 'default', // 'default', 'bordered'
   semiDark: false, // true, false
-  layout: 'horizontal', // 'vertical', 'collapsed', 'horizontal'
+  layout: 'vertical', // 'vertical', 'collapsed', 'horizontal'
   layoutPadding: 24, // Common padding for header, content, footer layout components (in px)
   compactContentWidth: 1440, // in px
   navbar: {

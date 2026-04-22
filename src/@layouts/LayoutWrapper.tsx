f199ef98a2e5e51b7b07f1eca80c8bac285c59bf
vovantung <vovantungdt123@gmail.com>
2026-04-22 21:42:00 +0700

    cm263

--- src/@layouts/LayoutWrapper.tsx
+++ src/@layouts/LayoutWrapper.tsx
@@ -4,6 +4,8 @@
 import type { ReactElement } from 'react'
 
 // Type Imports
+import { useMediaQuery, useTheme } from '@mui/material'
+
 import type { SystemMode } from '@core/types'
 
 // Hook Imports
@@ -14,9 +16,14 @@ type LayoutWrapperProps = {
   systemMode: SystemMode
   verticalLayout: ReactElement
   horizontalLayout: ReactElement
+
+  // layout: Layout
 }
 
 const LayoutWrapper = (props: LayoutWrapperProps) => {
+  const theme = useTheme()
+  const lgAbove = useMediaQuery(theme.breakpoints.up('lg'))
+
   // Props
   const { systemMode, verticalLayout, horizontalLayout } = props
 
@@ -28,7 +35,7 @@ const LayoutWrapper = (props: LayoutWrapperProps) => {
   // Return the layout based on the layout context
   return (
     <div className='flex flex-col flex-auto' data-skin={settings.skin}>
-      {settings.layout === 'horizontal' ? horizontalLayout : verticalLayout}
+      {settings.layout === 'horizontal' ? horizontalLayout : !lgAbove ? horizontalLayout : verticalLayout}
     </div>
   )
 }
